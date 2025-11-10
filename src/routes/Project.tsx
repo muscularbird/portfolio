062b51b8702b103a9a90cdfa604bd384df338f5d
muscularbird <etienne.kretz@protonmail.com>
2025-11-10 14:25:53 +0100

    [~] to build

--- src/routes/Project.tsx
+++ src/routes/Project.tsx
@@ -14,7 +14,7 @@ export default function Project() {
       <h1 className="text-4xl font-bold m-5">{project.title}</h1>
         <img src={project.images[0]} alt={project.title} className="w-100 h-auto m-5 rounded-xl" />
       <p className="mb-10 text-2xl w-9/10 text-justify">{project.description}</p>
-      {project.competences.map((competence, index) => (
+      {project?.competences?.map((competence, index) => (
         <span key={index} className="text-sm text-foreground rounded-full p-1 m-3 border-slate-900 border">
           {competence}
         </span>
